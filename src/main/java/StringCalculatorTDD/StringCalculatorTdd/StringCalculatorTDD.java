9c3bcd55f9b13736d7805d3455b862dc73622319
QaziFarheen <qazifarhin78@gmail.com>
2024-10-26 12:33:34 +0530

    TestAddEmptyString Pass

---
+++ src/main/java/StringCalculatorTDD/StringCalculatorTdd/StringCalculatorTDD.java
@@ -0,0 +1,8 @@
+package StringCalculatorTDD.StringCalculatorTdd;
+
+public class StringCalculatorTDD {
+    public int add(String numbers) {
+        return 0;
+    }
+
+}
--- src/test/java/StringCalculatorTDD/StringCalculatorTdd/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTDD/StringCalculatorTdd/StringCalculatorTest.java
@@ -2,13 +2,15 @@ package StringCalculatorTDD.StringCalculatorTdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
     @Test
-    public void testAddEmptyString() {
+   public  void testAddEmptyString() {
         StringCalculatorTDD calculator = new StringCalculatorTDD();
         assertEquals(0, calculator.add(""));
     }
 }
 
 
-}
+
