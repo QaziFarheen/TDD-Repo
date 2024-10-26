c452a3fbb664a78a0dd3ee7b9d2f42a0d7761501
QaziFarheen <qazifarhin78@gmail.com>
2024-10-26 12:15:17 +0530

    initial commit

---
+++ src/test/java/StringCalculatorTDD/StringCalculatorTdd/StringCalculatorTddApplicationTests.java
@@ -0,0 +1,13 @@
+package StringCalculatorTDD.StringCalculatorTdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
