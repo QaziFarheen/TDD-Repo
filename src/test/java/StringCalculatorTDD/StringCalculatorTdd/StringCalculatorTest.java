10dcf4f747f14bc710ceecde544c211a91da14d6
QaziFarheen <qazifarhin78@gmail.com>
2024-10-26 12:56:28 +0530

    test casese fail for negative num

--- src/test/java/StringCalculatorTDD/StringCalculatorTdd/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTDD/StringCalculatorTdd/StringCalculatorTest.java
@@ -3,6 +3,7 @@ package StringCalculatorTDD.StringCalculatorTdd;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringCalculatorTest {
     @Test
@@ -36,6 +37,14 @@ public class StringCalculatorTest {
         assertEquals(3, calculator.add("//;\n1;2"));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddNegativeNumbers() {
+        StringCalculatorTDD calculator = new StringCalculatorTDD();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.add("1,-2,3,-4");
+        });
+        assertEquals("negative numbers not allowed: -2, -4", exception.getMessage());
+    }
 
 
 
